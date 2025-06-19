256e4e62124bef7572a5ab2c2b3ae23a6a5377a8
Omer Mohammed <omermd529@gmail.com>
2025-06-19 14:50:08 +0530

    file2.js

--- file2.js
+++ file2.js
@@ -1,1 +1,3 @@
 drone
+Air India Ahmedabad Crash
+AI 171 from Ahmedabad to Gatwick London crashed a short while after takeoff. 241 out of 242 people died including 10 crew members that included 2 pilots.
